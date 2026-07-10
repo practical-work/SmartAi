90bdc16639c9a653b367d1f82dd3072e103805be
practical-work <62kunalvats@gmail.com>
2026-07-10 17:40:27 +0530

    made changes in app.py

--- LogisticRegressionModel/app.py
+++ LogisticRegressionModel/app.py
@@ -4,6 +4,7 @@ import joblib # or pickle
 from flask import Flask,render_template,redirect,request,url_for
 from sklearn.metrics import (accuracy_score,precision_score,recall_score,f1_score,confusion_matrix,classification_report)
 import nltk
+nltk.download("stopwords")
 import string
 from nltk.corpus import stopwords
 from nltk.stem import PorterStemmer
